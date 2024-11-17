5f06a687b0c3cd46a85ea54acf9f7f2a5f0684fd
Sheab Ali <sheabali@Sheab-MacBook-Pro.local>
2024-11-18 03:24:55 +0600

    first project level 2 course

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+// import { config } from "dotenv";
+
+import app from "./app";
+import config from "./app/config";
+
+const mongoose = require('mongoose');
+
+async function main() {
+
+  try{
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch(err){
+    console.log(err)
+  }
+
+    
+    
+   
+  }
+
