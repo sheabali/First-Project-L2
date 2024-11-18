8dbad2a9fcf6504eeeed74a388e7ddec7321362e
Sheab Ali <sheabali@Sheab-MacBook-Pro.local>
2024-11-18 23:06:34 +0600

    crated some file and folders

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,18 @@ export default [
     ignores: ["node_modules", "dist"],
     rules: {
       "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error"
     },
+    "globals": {
+      "process": "readonly"
+    }
   },
 ];
+
+
+
+
+
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import express, { Application, Request, Response } from "express"
-import cors  from "cors";
-const app: Application = express()
-const port = 3000
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+const port = 3000;
 
 //parsers
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
+const d2 = 1;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-console.log(process.cwd())
-
+console.log(process.cwd());
 
 export default app;
-
