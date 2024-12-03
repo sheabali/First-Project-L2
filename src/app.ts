671d40763f737ad589bc7dbf588a72857308a957
sheabali <iamskpranto@gmail.com>
2024-12-04 03:57:42 +0600

    motification

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,7 @@ import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/global.ErrorHandler';
 import notFound from './app/middlewares/notFound';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
+import { router } from './app/routes';
 
 const app: Application = express();
 
@@ -12,8 +11,15 @@ app.use(express.json());
 app.use(cors());
 console.log(app);
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
+
+const test = (req: Request, res: Response) => {
+  const a = 10;
+
+  res.send(a);
+};
+
+app.use('/', test);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
