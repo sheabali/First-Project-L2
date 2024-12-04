b3fbe69583b5e310ae7a7d35b5e82d6c4b21f260
sheabali <iamskpranto@gmail.com>
2024-12-05 01:52:58 +0600

    a

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,46 +1,36 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import sendResponse from '../../utils/sendResponse';
 import { studentsServices } from './student.service';
 
-const getAllStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await studentsServices.getAllStudentFromDB();
-
-    sendResponse(res, {
-      statusCode: StatusCodes.OK,
-      success: true,
-      message: 'student are retivieved successfully.',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
 };
 
-const getSingleStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { studentId } = req.params;
-    const result = await studentsServices.getOneStudentFromDB(studentId);
+const getSingleStudent = catchAsync(async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await studentsServices.getOneStudentFromDB(studentId);
 
-    sendResponse(res, {
-      statusCode: StatusCodes.OK,
-      success: true,
-      message: 'student retivieved successfully.',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'student retivieved successfully.',
+    data: result,
+  });
+});
+
+const getAllStudent = catchAsync(async (req, res, next) => {
+  const result = await studentsServices.getAllStudentFromDB();
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'student are retivieved successfully.',
+    data: result,
+  });
+});
 
 export const StudentControllers = {
   getAllStudent,
