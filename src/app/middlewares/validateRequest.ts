3d5de0b522a205ceee2cf1af5d14fea68329ed51
sheabali <iamskpranto@gmail.com>
2024-12-07 00:05:28 +0600

    create a higher order function

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,3 +1,15 @@
-const validateRequest = () => {
-  return async () => {};
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject } from 'zod';
+
+const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    //validate check
+
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
 };
+
+export default validateRequest;
