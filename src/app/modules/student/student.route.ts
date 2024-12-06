a2c3895cc23e59d48e60c799209ed61b0acc6343
sheabali <iamskpranto@gmail.com>
2024-12-06 23:17:08 +0600

    Implement Your Army Middleware

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,10 +1,15 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { StudentControllers } from './student.controller';
 
 const router = express.Router();
 
+const shenaBahini = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body);
+  next();
+};
+
 router.get('/', StudentControllers.getAllStudent);
 
-router.get('/:studentId', StudentControllers.getSingleStudent);
+router.get('/:studentId', shenaBahini, StudentControllers.getSingleStudent);
 
 export const StudentRoutes = router;
