722b6f45458c36e967fdb5f491a5825c52645504
sheabali <iamskpranto@gmail.com>
2024-12-08 17:45:56 +0600

    12-8 Handle Logical Validation of Academic Semester

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,12 @@
+import { academicSemesterNameCodeMapper } from './academicSemester.constant';
 import { AcademicSemester } from './academicSemester.model';
 import { TAcademicSemester } from './academicSmester.interface';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid Semester Code');
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
