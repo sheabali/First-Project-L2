9ee03d4818dcd3d8be5adfb8e455b01fa700e12f
sheabali <iamskpranto@gmail.com>
2024-12-11 22:25:38 +0600

    academic department service  ready

---
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -0,0 +1,37 @@
+import { TAcademicDepartment } from './academicDepartment.interface';
+import { AcademicDepartment } from './academicDepartment.model';
+
+const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
+  const result = await AcademicDepartment.create(payload);
+  return result;
+};
+
+const getAllAcademicDepartmentFromDB = async () => {
+  const result = await AcademicDepartment.find();
+  return result;
+};
+
+const getSingleAcademicDepartmentFromDB = async (_id: string) => {
+  const result = await AcademicDepartment.findOne({ _id });
+
+  return result;
+};
+
+const updateAcademicDepartmentFromDB = async (
+  id: string,
+  payload: Partial<TAcademicDepartment>,
+) => {
+  const updateAcademicDepartment = await AcademicDepartment.findByIdAndUpdate(
+    { _id: id },
+    payload,
+    { new: true },
+  );
+  return updateAcademicDepartment;
+};
+
+export const AcademicDepartmentService = {
+  createAcademicDepartmentIntoDB,
+  getAllAcademicDepartmentFromDB,
+  getSingleAcademicDepartmentFromDB,
+  updateAcademicDepartmentFromDB,
+};
