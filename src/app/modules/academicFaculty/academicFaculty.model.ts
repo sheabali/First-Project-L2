8c07b09ea76073affc33c06a47fa471b75f5af0f
sheabali <iamskpranto@gmail.com>
2024-12-12 01:35:23 +0600

    more validated academic faculty create and update

--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -14,6 +14,28 @@ const academicFacultyschema = new Schema<TAcademicFaculty>(
   },
 );
 
+academicFacultyschema.pre('save', async function (next) {
+  const isFacultyExist = await AcademicFaculty.findOne({
+    name: this.name,
+  });
+
+  if (isFacultyExist) {
+    throw new Error('This Faculty is already exist!');
+  }
+  next();
+});
+
+academicFacultyschema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+
+  const isFacultyExist = await AcademicFaculty.findOne(query);
+
+  if (!isFacultyExist) {
+    throw new Error('This Faculty dose not exist!');
+  }
+  next();
+});
+
 export const AcademicFaculty = model<TAcademicFaculty>(
   'AcademicFaculty',
   academicFacultyschema,
