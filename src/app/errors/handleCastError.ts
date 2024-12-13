a369b460493dd7078374efce5cf0205fcd03cce7
sheabali <iamskpranto@gmail.com>
2024-12-13 20:33:12 +0600

    CastError

---
+++ src/app/errors/handleCastError.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import { TErrorSources, TGenericErrorRespons } from '../interface/error';
+
+const handleCastError = (
+  err: mongoose.Error.CastError,
+): TGenericErrorRespons => {
+  const errorSources: TErrorSources = [
+    {
+      path: err.name,
+      message: err.message,
+    },
+  ];
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'CastError Error.',
+    errorSources,
+  };
+};
+
+export default handleCastError;
--- src/app/middlewares/global.ErrorHandler.ts
+++ src/app/middlewares/global.ErrorHandler.ts
@@ -1,6 +1,7 @@
 import { ErrorRequestHandler } from 'express';
 import { ZodError } from 'zod';
 import config from '../config';
+import handleCastError from '../errors/handleCastError';
 import handleValidationError from '../errors/handleValidationError';
 import handleZodError from '../errors/handleZodError';
 import { TErrorSources } from '../interface/error';
@@ -24,6 +25,11 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorSource = simplifiedError?.errorSources;
   } else if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err);
+    (statusCode = simplifiedError?.statusCode),
+      (message = simplifiedError?.message);
+    errorSource = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
     (statusCode = simplifiedError?.statusCode),
       (message = simplifiedError?.message);
     errorSource = simplifiedError?.errorSources;
