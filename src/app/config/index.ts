06005c2ceaa38d7e8f807b1e6bf0952ab6be4a59
sheabali <iamskpranto@gmail.com>
2024-12-13 15:13:10 +0600

    lunch time

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   port: process.env.PORT,
-
+  NODE_ENV: process.env.NODE_ENV,
   database_url: process.env.DATABASE_URL,
   default_password: process.env.DEFAULT_PASS,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
--- src/app/middlewares/global.ErrorHandler.ts
+++ src/app/middlewares/global.ErrorHandler.ts
@@ -2,21 +2,52 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import config from '../config';
+import { TErrorSource } from '../interface/error';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  const handleZodError = (err: ZodError) => {
+    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1] || '',
+        message: issue.message,
+      };
+    });
+
+    const statusCode = 400;
+
+    return {
+      statusCode,
+      message: 'Validation Error.',
+      errorSources,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+
+    (statusCode = simplifiedError?.statusCode),
+      (message = simplifiedError?.message),
+      (errorSource = simplifiedError?.errorSources);
+  }
 
   res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSource,
+    stack: config.NODE_ENV === 'development' ? err.stack : null,
   });
 };
 
