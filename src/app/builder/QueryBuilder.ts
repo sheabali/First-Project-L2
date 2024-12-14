894090adcc699e6e915571bd49bbbab75f52e908
sheabali <iamskpranto@gmail.com>
2024-12-14 21:59:08 +0600

    leraning successfuly

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -1,1 +1,10 @@
-class QueryBuilder<T> {}
+import { Query } from 'mongoose';
+
+class QueryBuilder<T> {
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    (this.modelQuery = modelQuery), (this.query = query);
+  }
+}
