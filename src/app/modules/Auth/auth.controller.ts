9bcf36c4160bf850c6b729019f8b5a6aa9b550cd
sheabali <iamskpranto@gmail.com>
2024-12-19 23:13:18 +0600

    auth route

---
+++ src/app/modules/Auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import { StatusCodes } from 'http-status-codes';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+
+const loginUser = catchAsync(async (req, res) => {
+  const result = await AuthServices.loginUser(req.body);
+  const { refreshToken, accessToken, needsPasswordChange } = result;
+
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  });
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'User is logged in succesfully!',
+    data: {
+      accessToken,
+      needsPasswordChange,
+    },
+  });
+});
+
+export const AuthControllers = {
+  loginUser,
+};
