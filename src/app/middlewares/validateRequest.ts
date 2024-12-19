a2788fdd9af7fdf67a1ae201220bf596a5453282
sheabali <iamskpranto@gmail.com>
2024-12-20 02:50:49 +0600

    some change

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,20 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject } from 'zod';
+import catchAsync from '../utils/catchAsync';
 
 const validateRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     //validate check
+    await schema.parseAsync({
+      body: req.body,
+    });
 
-    try {
-      await schema.parseAsync({
-        body: req.body,
-      });
-
-      next();
-    } catch (err) {
-      next(err);
-    }
-  };
+    next();
+  });
 };
 
 export default validateRequest;
