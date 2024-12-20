c6108cc7bab18b01cc224da98d1b21149ac88464
sheabali <iamskpranto@gmail.com>
2024-12-21 01:00:41 +0600

    Add password changed validation into auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -4,6 +4,7 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import AppError from '../errors/AppError';
 import { TUserRole } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
 import catchAsync from '../utils/catchAsync';
 
 const auth = (...requiredRoles: TUserRole[]) => {
@@ -17,33 +18,59 @@ const auth = (...requiredRoles: TUserRole[]) => {
       throw new AppError(StatusCodes.UNAUTHORIZED, 'You are not authorized!');
     }
 
-    // invalid token
-    jwt.verify(
+    const decoded = jwt.verify(
       token,
       config.jwt_access_secret as string,
-      function (err, decoded) {
-        if (err) {
-          throw new AppError(
-            StatusCodes.UNAUTHORIZED,
-            'You are not authorized!',
-          );
-        }
-        console.log(decoded);
-        const role = (decoded as JwtPayload).data.role;
-        console.log('role', role);
+    ) as JwtPayload;
 
-        console.log(!requiredRoles.includes(role));
-        if (requiredRoles && !requiredRoles.includes(role)) {
-          throw new AppError(
-            StatusCodes.UNAUTHORIZED,
-            'You are not authorizedd!',
-          );
-        }
+    // invalid token
 
-        req.user = decoded as JwtPayload;
-        next();
-      },
-    );
+    console.log(decoded);
+    // const role = decoded.data.role;
+    // console.log('role', role);
+
+    const { role, userId, iat } = decoded.data;
+    const user = await User.isUserExistsByCustomId(userId);
+
+    if (!user) {
+      throw new AppError(StatusCodes.NOT_FOUND, 'This user is not found !');
+    }
+    // checking if the user is already deleted
+
+    const isDeleted = user?.isDeleted;
+
+    if (isDeleted) {
+      throw new AppError(StatusCodes.FORBIDDEN, 'This user is deleted !');
+    }
+
+    // checking if the user is blocked
+    // Pi and Richard Parker
+
+    const userStatus = user?.status;
+
+    if (userStatus === 'blocked') {
+      throw new AppError(StatusCodes.FORBIDDEN, 'This user is blocked ! !');
+    }
+
+    if (
+      user.passwordChangedAt &&
+      User.isJWTIssuedBeforePasswordChanged(
+        user.passwordChangedAt,
+        iat as number,
+      )
+    ) {
+      throw new AppError(
+        StatusCodes.FORBIDDEN,
+        'You are not  user is authorized !',
+      );
+    }
+    console.log(!requiredRoles.includes(role));
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(StatusCodes.UNAUTHORIZED, 'You are not authorized!');
+    }
+
+    req.user = decoded as JwtPayload;
+    next();
   });
 };
 
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -70,4 +70,13 @@ userSchema.statics.isPasswordMatched = async function (
   return await bcrypt.compare(plainTextPassword, hashedPassword);
 };
 
+userSchema.statics.isJWTIssuedBeforePasswordChanged = function (
+  passwordChangedTimestamp: Date,
+  jwtIssuedTimestamp: number,
+) {
+  const passwordChangedTime =
+    new Date(passwordChangedTimestamp).getTime() / 1000;
+  return passwordChangedTime > jwtIssuedTimestamp;
+};
+
 export const User = model<TUser, UserModel>('User', userSchema);
